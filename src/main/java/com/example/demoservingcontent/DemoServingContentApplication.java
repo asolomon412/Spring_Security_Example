9e05e9d664cfdbedf22f4531b687e25159fb3b04
Antonella Solomon <antonellasolomon@gmail.com>
2018-03-27 15:26:29 -0400

    inital commit spring security example

---
+++ src/main/java/com/example/demoservingcontent/DemoServingContentApplication.java
@@ -0,0 +1,12 @@
+package com.example.demoservingcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoServingContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoServingContentApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/demoservingcontent/DemoServingContentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demoservingcontent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoServingContentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
